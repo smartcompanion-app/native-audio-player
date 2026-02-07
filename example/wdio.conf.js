37d480d030acb0205acf8d1a22f70cf6daa07a92
Stefan Huber <mail@stefanhuber.at>
2026-02-07 22:22:31 +0100

    feat: test ios

--- example/wdio.conf.js
+++ example/wdio.conf.js
@@ -1,9 +1,20 @@
+const isIOS = process.env.APPIUM_PLATFORM === 'ios';
+
 export const config = {
   runner: 'local',
   framework: 'mocha',
   port: 4723,
-  capabilities: [
-    {
+  capabilities: [    
+    isIOS ? {
+      platformName: 'iOS',
+      'appium:app': './ios/build/Build/Products/Debug-iphonesimulator/App.app',
+      'appium:automationName': 'XCUITest',
+      'appium:deviceName': 'iPhone 16',
+      'appium:platformVersion': '18.2',
+      'appium:bundleId': 'app.smartcompanion.audio.plugin.example',
+      'appium:useNewWDA': false,
+      'appium:showXcodeLog': true,
+    } : {
       platformName: 'Android',
       'appium:app': './android/app/build/outputs/apk/debug/app-debug.apk',
       'appium:automationName': 'UiAutomator2',
@@ -16,7 +27,7 @@ export const config = {
   services: [
     ['appium', {
       args: {
-        'allow-insecure': 'uiautomator2:chromedriver_autodownload',
+        'allow-insecure': '*:chromedriver_autodownload',
       }
     }]
   ],
